4fd2ee77d51ec9e0b2ea377806a53396970eb73b
Jeremy Buentello <jb09056@georgiasouthern.edu>
2024-02-04 20:41:06 -0500

    Adding the ChainGeneratorPageModel for future proofing some things

---
+++ ChainGenerator/Models/ChainGeneratorPageModel.cs
@@ -0,0 +1,11 @@
+﻿using ChainGenerator.Data;
+
+namespace ChainGenerator.Models
+{
+    public class ChainGeneratorPageModel
+    {
+        public string Title { get; set; }  // The title of the generator page
+        public List<WidgetGeneratorModel> WidgetGeneratorModels { get; set; } = new();
+        public ApplicationUser Owner { get; set; }
+    }
+}
